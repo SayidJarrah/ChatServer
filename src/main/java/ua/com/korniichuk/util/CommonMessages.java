61b5b8d1c6f15b4db5c5bb4d554eef891d45022d
Dima <dkorniychuk91@gmail.com>
2016-02-11 14:16:16 +0200

    changes

---
+++ src/main/java/ua/com/korniichuk/util/CommonMessages.java
@@ -0,0 +1,16 @@
+package ua.com.korniichuk.util;
+
+
+import ua.com.korniichuk.client.ClientRepository;
+
+public class CommonMessages {
+    public static final String WELCOME = "Welcome in myChat 1.0" + System.getProperty("line.separator") +
+            "Now " + ClientRepository.getInstance().size() + " users online" +
+            System.getProperty("line.separator") +
+            "Enter, pls, your nick:" + System.getProperty("line.separator");
+
+    public static final String ONLINE = " online";
+    public static final String OFFLINE = " left chat";
+    public static final String STATUS = "Now " + ClientRepository.getInstance().size() + " users online";
+
+}
